91f12a1cf9e8cd0b06d78702fd5cae20eed966a0
elibeta22 <ebeta922@gmail.com>
2022-11-09 12:46:07 -0500

    Cleaned up repo
    Cleaned up repo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { NewsComponent } from './news/news.component';
-import { PostListComponent } from './post-list/post-list.component';
+
 const routes: Routes = [
   { path: 'news', component: NewsComponent},
-  { path: 'postlist', component: PostListComponent},
   { path: 'login', component: LoginComponent},
   { path: 'home', component: HomeComponent},
 
