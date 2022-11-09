203dc2071c86cc322ab74cf9a3b916ee032d34e5
elibeta22 <ebeta922@gmail.com>
2022-11-09 09:40:20 -0500

    initial commit
    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { NewsComponent } from './news/news.component';
+import { PostListComponent } from './post-list/post-list.component';
+const routes: Routes = [
+  { path: 'news', component: NewsComponent},
+  { path: 'postlist', component: PostListComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'home', component: HomeComponent},
 
-const routes: Routes = [];
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewsComponent } from './news/news.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PostComponent } from './post/post.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { LoginComponent } from './login/login.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { HomePostComponent } from './home-post/home-post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsComponent,
+    NavbarComponent,
+    PostComponent,
+    PostListComponent,
+    LoginComponent,
+    FooterComponent,
+    HomeComponent,
+    HomePostComponent
   ],
   imports: [
     BrowserModule,
